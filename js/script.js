2d2d88387096404eeaa1379ca98b80bb3ab83518
Akas_Datta <012akasdatta@gmail.com>
2025-03-31 02:39:14 +0600

    added time count

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Banner image code 
+// Banner image code //
 const mainImage = document.getElementById("mainImage");
 const buttons = document.querySelectorAll("button");
 
@@ -15,3 +15,13 @@ buttons.forEach(button => {
         this.classList.add("border-4");
     });
 });
+
+
+
+// Time count down //
+let timer = 28 * 60, display = document.getElementById("countdown");
+setInterval(() => {
+  let m = Math.floor(timer / 60), s = timer % 60;
+  display.textContent = `${m}:${s < 10 ? "0" : ""}${s} min avant la fin de l'offre`;
+  timer > 0 ? timer-- : display.textContent = "Offre expirée";
+}, 1000);
